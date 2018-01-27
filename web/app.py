f20e0a6f9c766c4a4f112e0f3d1d2827907a5766
Brandon Do <brandondo64@gmail.com>
2018-01-26 19:17:11 -0800

    Add files via upload

--- web/app.py
+++ web/app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+def print_files():
+    """
+    Helper function to print current directories.
+    """
+    for dirpath, dirnames, filenames in os.walk(os.getcwd()):
+        print("Current Path:", dirpath)
+        print("Directories:", dirnames)
+        print("Files:", filenames)
+        print()
+
+def get_html():
+    for dirpath, dirnames, filenames in os.walk(os.getcwd()):
+        print(dirpath)
+        if not dirpath.endswith('error_files'):
+            for filename in filenames:
+                if filename.endswith('.html'):
+                    if filename.startswith('..') or filename.startswith('~') or filename.startswith('//'):
+                        return "error_files/403.html"
+                    else:
+                        return filename
+    return "error_files/404.html"
+
 @app.route("/")
-def hello():
-    return "UOCIS docker demo!"
+@app.route('/templates')
+def render_static():
+    file = get_html()
+    print(file)
+    return render_template(file)
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
